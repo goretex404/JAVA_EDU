df737a5a2a5ab72e429389327b5ac12b5d9da2a1
allin <goretex404@gmail.com>
2023-03-18 21:14:44 +0900

    프로젝트 생성

---
+++ src/chap1/Exam_01_02.java
@@ -0,0 +1,13 @@
+package chap1;
+
+public class Exam_01_02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1 = 10;
+		double d1 = 10.1;
+		
+		System.out.println(num1 + d1);
+	}
+
+}
